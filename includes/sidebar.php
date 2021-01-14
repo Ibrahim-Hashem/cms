8a4552518fba48d60655004f01c5d4da35dfd2b1
Ibrahim Hashem <ibrahimsgh6@hotmail.co.uk>
2021-01-14 15:54:31 +0000

    search function

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,17 +1,28 @@
 <!-- Blog Sidebar Widgets Column -->
 <div class="col-md-4">
+<?php
+    if(isset($_POST['search'])){
+       echo $search = $_POST['search']; 
+    }else{
+        echo "";
+    }
 
+?>
     <!-- Blog Search Well -->
     <div class="well">
         <h4>Blog Search</h4>
+        <form action="" method="post">
+            
         <div class="input-group">
-            <input type="text" class="form-control">
+            <input type="text" name="search" class="form-control">
             <span class="input-group-btn">
-                <button class="btn btn-default" type="button">
+            <button class="btn btn-default" type="submit" name="submit">
                     <span class="glyphicon glyphicon-search"></span>
             </button>
             </span>
         </div>
+        
+        </form>
         <!-- /.input-group -->
     </div>
 
