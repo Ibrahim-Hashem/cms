e564d64d6f84784aefebc7574926ca974d88a63f
Ibrahim Hashem <ibrahimsgh6@hotmail.co.uk>
2021-01-15 17:38:36 +0000

    admin categories title change

--- admin/categories.php
+++ admin/categories.php
@@ -9,8 +9,7 @@
         <div class="row">
             <div class="col-lg-12">
                 <h1 class="page-header">
-                    Welcome to Admin Page
-                    <small>Author</small>
+                    Categories
                 </h1>
             </div>
             <div class="col-xs-6">
