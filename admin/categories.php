fa5dd513614f45a51448f578f0c904fadbf97cd4
Ibrahim Hashem <ibrahimsgh6@hotmail.co.uk>
2021-01-19 13:55:51 +0000

    update feature complete

--- admin/categories.php
+++ admin/categories.php
@@ -61,12 +61,23 @@
                                     <input type="text" name="cat_title" class="form-control" value="<?php echo $cat_title; ?>">
                                     
                                 <?php } ?>
-                            
                     <?php } ?>
+                    
+                    <?php
+                        if(isset($_POST['update_category'])){
+                            $the_update_title = $_POST['cat_title'];
+                            $edit_query = "UPDATE categories SET cat_title = '{$the_update_title}' WHERE cat_id = '{$cat_id}' ";
+                            $update_query_connect = mysqli_query($connect, $edit_query); 
+                            header("Location: categories.php");
+                        }    
+                        
+                        
+                    ?>
+                    
                     </div>
                     <div class="form-group">
                     
-                        <input type="submit" class="btn btn-primary" name="edit" value="Edit Category">
+                        <input type="submit" class="btn btn-primary" name="update_category" value="Edit Category">
                     </div>
                 </form>
             </div>
