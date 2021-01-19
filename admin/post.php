e6f43fe2bf6f6280592a2710ef14aa82f9dcfa2d
Ibrahim Hashem <ibrahimsgh6@hotmail.co.uk>
2021-01-19 16:25:20 +0000

    dynamic post tabel

--- admin/post.php
+++ admin/post.php
@@ -11,7 +11,7 @@
                 <h1 class="page-header">
                     Posts
                 </h1>
-                <div class="col-xs-6">
+
                    
                     <table class="table table-bordered table-hover">
                         <thead>
@@ -28,24 +28,36 @@
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td>2</td>
-                                <td>james</td>
-                                <td>php language</td>
-                                <td>php</td>
-                                <td>status</td>
-                                <td>image</td>
-                                <td>tags</td>
-                                <td>comments</td>
-                                <td>date</td>
-                            </tr>
+                            <?php
+                                $query = "SELECT * FROM posts";
+                                $query_connect = mysqli_query($connect,$query);
+                                
+                                while($row = mysqli_fetch_assoc($query_connect)){
+                                    $post_id = $row['post_id'];
+                                    $post_author = $row['post_author'];
+                                    $post_title = $row['post_title'];
+                                    $post_category_id = $row['post_category_id'];
+                                    $post_status = $row['post_status'];
+                                    $post_image = $row['post_image'];
+                                    $post_tags = $row['post_tags'];
+                                    $post_comments = $row['post_comment_count'];
+                                    $post_date = $row['post_date'];
+                                    ?>
+                                    
+                                    <tr>        
+                                        <td><?php echo $post_id;  ?></td>
+                                        <td><?php echo $post_author;  ?></td>
+                                        <td><?php echo $post_title;  ?></td>
+                                        <td><?php echo $post_category_id;  ?></td>
+                                        <td><?php echo $post_status;  ?></td>
+                                        <td><img src="../images/<?php echo $post_image;?>" style="width:200px;" alt=""></td>
+                                        <td><?php echo $post_tags;  ?></td>
+                                        <td><?php echo $post_comments;  ?></td>
+                                        <td><?php echo $post_date;  ?></td>
+                                    </tr> 
+                                <?php }; ?>
                         </tbody>
                     </table>
-                </div>
-                
-                <div class="col-xs-6">
-                    
-                </div>
                 
             </div> 
         </div>
