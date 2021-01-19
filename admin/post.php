8f742a02c777e309769b83d785f372c84f8bd1df
Ibrahim Hashem <ibrahimsgh6@hotmail.co.uk>
2021-01-19 15:36:51 +0000

    table for post created

--- admin/post.php
+++ admin/post.php
@@ -11,11 +11,46 @@
                 <h1 class="page-header">
                     Posts
                 </h1>
+                <div class="col-xs-6">
+                   
+                    <table class="table table-bordered table-hover">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Author</th>
+                                <th>Title</th>
+                                <th>Categories</th>
+                                <th>Status</th>
+                                <th>Image</th>
+                                <th>Tags</th>
+                                <th>Comments</th>
+                                <th>Date</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>2</td>
+                                <td>james</td>
+                                <td>php language</td>
+                                <td>php</td>
+                                <td>status</td>
+                                <td>image</td>
+                                <td>tags</td>
+                                <td>comments</td>
+                                <td>date</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                
+                <div class="col-xs-6">
+                    
+                </div>
+                
             </div> 
         </div>
         <!-- /.row -->
         
-
     </div>
     <!-- /.container-fluid -->
 
