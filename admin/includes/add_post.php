763dda05e9e6594134671f3a965af627c3c45229
Ibrahim Hashem <ibrahimsgh6@hotmail.co.uk>
2021-01-20 12:12:19 +0000

    add function made

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -15,6 +15,16 @@
         
         move_uploaded_file($post_image_temp,"../images/$post_image");
         
+        $query = "INSERT INTO posts(post_category_id, post_title, post_author, post_date, post_image, post_content, post_tags, post_comment_count ,post_status) ";
+        $query .= "VALUES({$post_cat_id},'{$post_title}', '{$post_author}', now(), '{$post_image}','{$post_content}', '{$post_tags}','{$post_comment_count}', '{$post_status}')";
+        
+        $create_post_query = mysqli_query($connect,$query);
+        
+        if($create_post_query){
+            echo "yes";
+        }else{
+            echo "no";
+        }
         
     }
  ?> 
