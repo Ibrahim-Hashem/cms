71f684272606247e0e8453aadc7b8e5b8ba22433
Ibrahim Hashem <ibrahimsgh6@hotmail.co.uk>
2021-01-28 14:45:29 +0000

    some style changes to the index cms page

--- index.php
+++ index.php
@@ -26,20 +26,26 @@
                     $post_content = substr($row['post_content'],0,50);
                     
                     ?>
-
                 <!-- First Blog Post -->
-                <h2>
-                    <a href="post.php?p_id=<?php echo "{$post_id}"?>"><?php echo "{$post_title}"?></a>
-                </h2>
-                <p class="lead">
-                    by <a href="index.php"><?php echo "{$post_author}"?></a>
-                </p>
-                <p><span class="glyphicon glyphicon-time"></span> <?php echo "{$post_date}"?></p>
-                <hr>
-                <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
-                <hr>
-                <p><?php echo "{$post_content}"?></p>
-                <a href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+                <div style="box-shadow: 1px 1px 15px #778899; border-radius:5px;">
+                   <div style="padding:10px; margin-bottom:50px;">
+                        <a style="text-decoration:none" href="post.php?p_id=<?php echo "{$post_id}"?>">
+                            <h2>
+                                <?php echo "{$post_title}"?>
+                            </h2>
+                            <p class="lead">
+                                by <?php echo "{$post_author}"?>
+                            </p>
+                            <p><span class="glyphicon glyphicon-time"></span> <?php echo "{$post_date}"?></p>
+                            <hr>
+                                <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
+
+                            <hr>
+                            <p><?php echo "{$post_content}"?></p>
+                            <p>Read More <span class="glyphicon glyphicon-chevron-right"></span></p> 
+                        </a>
+                    </div>
+                </div>
                 
             
             <?php } ?>
