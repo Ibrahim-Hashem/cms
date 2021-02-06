589fa46c22ccac899d16317dfe12b8f41e58085a
Ibrahim Hashem <ibrahimsgh6@hotmail.co.uk>
2021-02-06 21:52:35 +0000

    edit function completed

--- admin/includes/edit_user.php
+++ admin/includes/edit_user.php
@@ -2,19 +2,18 @@
 
 if(isset($_GET['edit_user'])){
     $the_user_id = $_GET['edit_user'];
-    
     $query = "SELECT * FROM users WHERE user_id = $the_user_id ";
     $query_users = mysqli_query($connect,$query);
 
     while($row = mysqli_fetch_assoc($query_users)){
-        $user_id = $row['user_id'];
-        $user_password = $row['user_password'];
-        $user_username = $row['username'];
-        $user_firstname = $row['user_firstname'];
-        $user_lastname = $row['user_lastname'];
-        $user_email = $row['user_email'];
-        $user_image = $row['user_image'];
-        $user_role = $row['user_role'];
+        $user_id_get = $row['user_id'];
+        $user_password_get = $row['user_password'];
+        $user_username_get = $row['username'];
+        $user_firstname_get = $row['user_firstname'];
+        $user_lastname_get = $row['user_lastname'];
+        $user_email_get = $row['user_email'];
+        $user_image_get = $row['user_image'];
+        $user_role_get = $row['user_role'];
     }
 }
 
@@ -38,7 +37,7 @@ if(isset($_POST['edit_user'])){
     move_uploaded_file($user_image_temp,"../images/$user_image");
 
    
-    $query_update = "UPDATE users SET username='{$user_username}', user_password='{$user_password}',user_firstname='{$user_firstname}', user_lastname ='{$user_lastname}',`user_email`='{$user_email}', user_image='{$user_image}',user_role='{$user_role}', randSalt = '' WHERE user_id={$user_id}";
+    $query_update = "UPDATE users SET username='{$user_username}', user_password='{$user_password}',user_firstname='{$user_firstname}', user_lastname ='{$user_lastname}',`user_email`='{$user_email}', user_image='{$user_image}',user_role='{$user_role}', randSalt = '' WHERE user_id={$the_user_id}";
     
 
     $edit_user_query = mysqli_query($connect,$query_update);
@@ -53,26 +52,26 @@ if(isset($_POST['edit_user'])){
 <form action="" method="post" enctype="multipart/form-data">
     <div class="form-group">
         <label for="user_firstname">First Name</label>
-        <input type="text" class="form-control" name="user_firstname" value="<?php echo "$user_firstname" ; ?>">
+        <input type="text" class="form-control" name="user_firstname" value="<?php echo "$user_firstname_get"; ?>">
     </div>
     
     <div class="form-group">
         <label for="user_lastname">Last Name</label>
-        <input type="text" class="form-control" name="user_lastname" value="<?php echo "$user_lastname"; ?>">
+        <input type="text" class="form-control" name="user_lastname" value="<?php echo "$user_lastname_get"; ?>">
     </div>   
     
        
     <div class="form-group">
         <label for="user_username">Username</label>
-        <input type="text" class="form-control" name="user_username" value="<?php echo "$user_username"; ?>">
+        <input type="text" class="form-control" name="user_username" value="<?php echo "$user_username_get"; ?>">
     </div>
-       
+        
     <div class="form-group">
         <label for="user_role">Role</label>
         <select name="user_role" class="form-control" style="width:200px;" id="" value="<?php echo "$user_role"; ?>">
-            <option value="subscriber"><?php echo "$user_role"; ?></option>
+            <option value="subscriber"><?php echo "$user_role_get"; ?></option>
             <?php 
-                if($user_role =='admin'){
+                if($user_role_get =='admin'){
                     echo "<option value='subscriber'>subscriber</option>";
                     
                 }else{
@@ -85,17 +84,17 @@ if(isset($_POST['edit_user'])){
        
     <div class="form-group">
         <label for="user_image">Image</label>
-        <img width="200" src="../images/<?php echo $user_image ?>" alt="">
+        <img width="200" src="../images/<?php echo $user_image_get ?>" alt="">
         <input type="file" class="form-contol"  name="user_image">
     </div>
     
     <div class="form-group">
         <label for="user_email">Email</label>
-        <input type="text" class="form-control" name="user_email" value="<?php echo "$user_email"; ?>">
+        <input type="text" class="form-control" name="user_email" value="<?php echo "$user_email_get"; ?>">
     </div>
     <div class="form-group">
         <label for="user_password">Password</label>
-        <input type="password" class="form-control" name="user_password" value="<?php echo "$user_password"; ?>">
+        <input type="password" class="form-control" name="user_password" value="<?php echo "$user_password_get"; ?>">
     </div>
        
     <div class="form-group">
