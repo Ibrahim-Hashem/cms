503a0217deade5f556729bd0b2371f0603d420ef
Ibrahim Hashem <ibrahimsgh6@hotmail.co.uk>
2021-02-09 11:43:26 +0000

    login form

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -21,17 +21,20 @@
     </div>
     <!--login-->
     <div class="well">
-        <h4>Blog Search</h4>
-        <form action="search.php" method="post">
-            
-        <div class="input-group">
-            <input type="text" name="search" class="form-control">
-            <span class="input-group-btn">
-            <button class="btn btn-default" type="submit" name="submit">
-                    <span class="glyphicon glyphicon-search"></span>
-            </button>
-            </span>
-        </div>
+        <h4>Login</h4>
+        <form action="includes/login.php" method="post">
+        <div class="form-group">
+                <input type="text" name="username" class="form-control" placeholder="Enter username">
+        </div> 
+              
+            <div class="input-group">
+                <input type="password" name="password" class="form-control" placeholder="Enter password">
+                <span class="input-group-btn">
+                    <button class="btn btn-primary" name="login" type="submit">
+                        button
+                    </button>
+                </span>
+            </div>
         
         </form>
         <!-- /.input-group -->
