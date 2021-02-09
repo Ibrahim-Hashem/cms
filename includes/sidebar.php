6f9007021529a037b1c5728fd7c740b37dcc3745
Ibrahim Hashem <ibrahimsgh6@hotmail.co.uk>
2021-02-09 10:52:00 +0000

    sidebar login

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -19,6 +19,23 @@
         </form>
         <!-- /.input-group -->
     </div>
+    <!--login-->
+    <div class="well">
+        <h4>Blog Search</h4>
+        <form action="search.php" method="post">
+            
+        <div class="input-group">
+            <input type="text" name="search" class="form-control">
+            <span class="input-group-btn">
+            <button class="btn btn-default" type="submit" name="submit">
+                    <span class="glyphicon glyphicon-search"></span>
+            </button>
+            </span>
+        </div>
+        
+        </form>
+        <!-- /.input-group -->
+    </div>
     
     <div class="well">
         <h4>Blog Categories</h4>
